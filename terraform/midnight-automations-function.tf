524922036e54ed33d2c88fa93042d74c474b60fb
Simon Knittel <hallo@simonknittel.de>
2025-10-12 13:28:41 +0200

    feat(midnight automations): deploy AWS Lambda function 3

--- terraform/midnight-automations-function.tf
+++ terraform/midnight-automations-function.tf
@@ -4,6 +4,6 @@ module "midnight_automations" {
   function_name         = "midnight-automations"
   account_id            = data.aws_caller_identity.current.account_id
   timeout               = 180
-  schedule_expression   = "cron(* 0 * * ? *)"
+  schedule_expression   = "cron(0 0 * * ? *)"
   scheduler_state       = "DISABLED"
 }
