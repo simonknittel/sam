a38ad0863f65cfa3a6dba2b6996369ad95d2ccf4
Simon Knittel <hallo@simonknittel.de>
2025-10-23 18:04:29 +0200

    docs: add explainer to custom Link wrapper

--- app/src/modules/common/components/Link.tsx
+++ app/src/modules/common/components/Link.tsx
@@ -5,6 +5,17 @@ import { useState, type ComponentProps } from "react";
 
 type Props = ComponentProps<typeof NextLink>;
 
+/**
+ * This component wraps Next.js' `<Link>` component while overriding its default
+ * prefetch behavior.
+ *
+ * By default, Next.js prefetches pages linked with the `<Link>` component when
+ * they enter the viewport. This can lead to unnecessary network requests and
+ * increased data usage, especially on pages with many links.
+ *
+ * This custom `<Link>` component changes the default behavior to only prefetch
+ * when the user hovers over the link.
+ */
 export const Link = (props: Props) => {
   const { prefetch, onMouseEnter, ...rest } = props;
 
