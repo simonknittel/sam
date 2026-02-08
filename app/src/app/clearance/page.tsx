a87fdc9212ad501c7d53d462ff37cd313bdc897d
Simon Knittel <hallo@simonknittel.de>
2026-02-08 15:40:51 +0100

    feat(IAM): styling

--- app/src/app/clearance/page.tsx
+++ app/src/app/clearance/page.tsx
@@ -46,44 +46,74 @@ export default async function Page() {
           </p>
         </div>
 
-        <div className="h-[1px] bg-neutral-700 mt-4 w-2 ml-auto mr-auto" />
+        <details>
+          <summary className="mt-4 text-center text-neutral-500 text-xs hover:text-interaction-500 focus-visible:text-interaction-500 active:text-interaction-300 hover:underline focus-visible:underline active:underline cursor-pointer">
+            Details anzeigen
+          </summary>
 
-        <div className="mt-4 flex flex-col gap-4 items-center px-4">
-          <div className="flex flex-col gap-1 text-neutral-500 text-xs max-w-full">
-            <div className="flex gap-1">
-              <p className="font-bold flex-none w-28">Discord-ID:</p>
-              <p
-                className="flex-1 truncate"
-                title={authentication.session.discordId}
-              >
-                {authentication.session.discordId}
-              </p>
-            </div>
+          <div className="mt-4 flex flex-col gap-4 items-center px-4">
+            <section className="flex flex-col gap-2 text-neutral-500 text-xs max-w-full">
+              <div>
+                <p className="font-bold mb-1">Discord</p>
 
-            <div className="flex gap-1">
-              <p className="font-bold flex-none w-28">E-Mail-Adresse:</p>
-              <p
-                className="flex-1 truncate"
-                title={authentication.session.user.email || undefined}
-              >
-                {authentication.session.user.email}
-              </p>
-            </div>
+                <div className="flex gap-1">
+                  <p className="flex-none w-28">ID:</p>
+                  <p
+                    className="flex-1 truncate"
+                    title={authentication.session.discordId}
+                  >
+                    {authentication.session.discordId}
+                  </p>
+                </div>
+              </div>
 
-            <div className="flex gap-1">
-              <p className="font-bold flex-none w-28">Benutzer-ID:</p>
+              <div>
+                <p className="font-bold mb-1">Benutzer</p>
 
-              <p
-                className="flex-1 truncate"
-                title={authentication.session.user.id}
-              >
-                {authentication.session.user.id}
-              </p>
-            </div>
+                <div className="flex gap-1">
+                  <p className="flex-none w-28">User ID:</p>
+
+                  <p
+                    className="flex-1 truncate"
+                    title={authentication.session.user.id}
+                  >
+                    {authentication.session.user.id}
+                  </p>
+                </div>
+
+                <div className="flex gap-1">
+                  <p className="flex-none w-28">E-Mail-Adresse:</p>
+                  <p
+                    className="flex-1 truncate"
+                    title={authentication.session.user.email || undefined}
+                  >
+                    {authentication.session.user.email}
+                  </p>
+                </div>
+              </div>
+
+              <div>
+                <p className="font-bold mb-1">Citizen</p>
+
+                {authentication.session.entity ? (
+                  <div className="flex gap-1">
+                    <p className="flex-none w-28">Internal ID:</p>
+                    <p
+                      className="flex-1 truncate"
+                      title={authentication.session.entity.id}
+                    >
+                      {authentication.session.entity.id}
+                    </p>
+                  </div>
+                ) : (
+                  "-"
+                )}
+              </div>
+            </section>
+
+            <ClearanceLogout />
           </div>
-
-          <ClearanceLogout />
-        </div>
+        </details>
       </main>
 
       <div className="h-[1px] bg-neutral-700 mt-4 w-2" />
