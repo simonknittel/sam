5ab5936ad357114c02ec75fb501c377663c95510
Simon Knittel <hallo@simonknittel.de>
2026-03-05 08:32:16 +0100

    refactor: remove Vercel Analytics

--- app/src/modules/settings/components/AnalyticsCheckbox.tsx
+++ app/src/modules/settings/components/AnalyticsCheckbox.tsx
@@ -3,24 +3,9 @@
 import { useState, type ChangeEventHandler } from "react";
 
 export const AnalyticsCheckbox = () => {
-  const [valueVercelAnalytics, setValueVercelAnalytics] = useState(
-    localStorage.getItem("va-disable") === "true",
-  );
   const [valuePlausible, setValuePlausible] = useState(
     localStorage.getItem("plausible_ignore") === "true",
   );
-
-  const handleChangeVercelAnalytics: ChangeEventHandler<HTMLInputElement> = (
-    e,
-  ) => {
-    if (e.target.checked) {
-      localStorage.setItem("va-disable", "true");
-    } else {
-      localStorage.removeItem("va-disable");
-    }
-
-    setValueVercelAnalytics(e.target.checked);
-  };
 
   const handleChangePlausible: ChangeEventHandler<HTMLInputElement> = (e) => {
     if (e.target.checked) {
@@ -34,18 +19,6 @@ export const AnalyticsCheckbox = () => {
 
   return (
     <>
-      <div className="flex gap-2 items-center">
-        <input
-          type="checkbox"
-          id="va-disable"
-          name="va-disable"
-          onChange={handleChangeVercelAnalytics}
-          defaultChecked={valueVercelAnalytics}
-        />
-
-        <label htmlFor="va-disable">Vercel Analytics</label>
-      </div>
-
       <div className="flex gap-2 items-center">
         <input
           type="checkbox"
