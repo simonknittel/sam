09d856691caca58485cb981394c076a7ee60b2bf
Simon Knittel <hallo@simonknittel.de>
2026-03-22 19:23:12 +0100

    feat: implement pre-announcement for releases

--- app/src/modules/common/components/NewReleaseToast.tsx
+++ app/src/modules/common/components/NewReleaseToast.tsx
@@ -5,15 +5,33 @@
 import { useChannelsContext } from "@/modules/pusher/components/ChannelsContext";
 import { useEffect } from "react";
 import toast from "react-hot-toast";
-import { FaRocket } from "react-icons/fa";
+import { FaRocket, FaSpinner } from "react-icons/fa";
 import { bumpDocumentTitle } from "../utils/title";
 import { Button2 } from "./Button2";
+
+const releaseToastId = "release-notification";
 
 export const NewReleaseToast = () => {
   const { client } = useChannelsContext();
 
   useEffect(() => {
     const channel = client?.subscribe("releases");
+
+    channel?.bind("deploying", () => {
+      toast.loading(
+        () => (
+          <div className="flex gap-2">
+            <p>In wenigen Minuten wird eine neue Version vom SAM veröffentlicht.</p>
+          </div>
+        ),
+        {
+          id: releaseToastId,
+          duration: Infinity,
+          icon: <FaSpinner className="flex-none animate-spin" />,
+          className: "gap-2 [&>div[role='status']]:m-0 pointer-events-none",
+        },
+      );
+    });
 
     channel?.bind("new", () => {
       toast.success(
@@ -24,6 +42,7 @@ export const NewReleaseToast = () => {
           </div>
         ),
         {
+          id: releaseToastId,
           duration: Infinity,
           icon: <FaRocket className="flex-none" />,
           className: "gap-2 [&>div[role='status']]:m-0",
@@ -34,6 +53,7 @@ export const NewReleaseToast = () => {
     });
 
     return () => {
+      channel?.unbind("deploying");
       channel?.unbind("new");
       channel?.unsubscribe();
     };
