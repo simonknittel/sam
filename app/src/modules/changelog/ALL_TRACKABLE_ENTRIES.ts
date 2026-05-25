299e7514630239a09e43a47b2aedf3596638a0ff
Simon Knittel <hallo@simonknittel.de>
2026-05-25 20:05:54 +0200

    feat(Log Analyzer): add pattern for Contract Failed

--- app/src/modules/changelog/ALL_TRACKABLE_ENTRIES.ts
+++ app/src/modules/changelog/ALL_TRACKABLE_ENTRIES.ts
@@ -3,6 +3,7 @@ export const ALL_TRACKABLE_ENTRIES = [
   "2026-05-25-blueprint-freischaltungen",
   "2026-05-25-contract-angenommen",
   "2026-05-25-contract-abgeschlossen",
+  "2026-05-25-contract-fehlgeschlagen",
   "2026-05-25-verbindung-getrennt",
   "2026-05-25-hervorhebung-neuer-changelog-einträge",
 ] as const;
