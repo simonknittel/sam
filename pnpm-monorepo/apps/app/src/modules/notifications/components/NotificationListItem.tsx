1406cdecd9a18facbe89835995ede4c79457d779
Simon Knittel <hallo@simonknittel.de>
2026-08-09 20:34:19 +0200

    fix: styling

--- pnpm-monorepo/apps/app/src/modules/notifications/components/NotificationListItem.tsx
+++ pnpm-monorepo/apps/app/src/modules/notifications/components/NotificationListItem.tsx
@@ -35,14 +35,14 @@ export const NotificationListItem = ({
 
   return (
     <li
-      className="relative group/notification border-l border-l-transparent px-4 py-2 hover:bg-neutral-800/50 focus-within:bg-neutral-800/50"
+      className="relative group/notification px-4 py-2 hover:bg-neutral-800/50 focus-within:bg-neutral-800/50"
       data-unread-notification-id={
         trackReadOnView ? notification.id : undefined
       }
     >
       {isUnread && (
         <div
-          className="absolute left-0 top-0 bottom-0 w-px"
+          className="absolute left-0 top-0 bottom-0 w-0.5"
           style={{
             background: "linear-gradient(to bottom, #f59e0b, transparent)",
           }}
